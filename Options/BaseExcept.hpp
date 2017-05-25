53fc75a9079c671a21ee1617da6d394041441f8a
BRutan <rutan.benjamin@gmail.com>
2017-05-25 03:59:22 -0400

    Initial upload.

---
+++ Options/BaseExcept.hpp
@@ -0,0 +1,65 @@
+/* BaseExcept.hpp
+-Description:
+	*Base class for all exception types in the Options namespace.
+-State Variables:
+	*string message: Stored message detailing exception.
+-Member Functions:
+	// Constructors/Destructor:
+	*BaseExcept(const string&): Overloaded constructor. Set message. 
+	*BaseExcept(const BaseExcept&): Copy constructor.
+	*~BaseExcept():	Destructor.
+	// Misc. Methods:
+	*string DerivedMessage() const: Return message in base class to derived class.
+	*string GetMessage() const: PVMF to make BaseExcept an abstract base class.
+*/
+
+#ifndef BASEEXCEPT_HPP
+#define BASEEXCEPT_HPP
+
+#include<string>
+
+namespace Options
+{
+	namespace Exceptions
+	{
+		class BaseExcept
+		{
+		protected:
+			std::string message;
+		public:
+			////////////////////////////
+			// Constructors/Destructor:
+			////////////////////////////
+			BaseExcept(const std::string &in) : message(in)			/* Overloaded Constructor. Sets message. */
+			{
+
+			}
+			BaseExcept(const BaseExcept &in) : message(in.message)  /* Copy Constructor. */
+			{
+
+			}
+			virtual ~BaseExcept()									/* Destructor. */
+			{
+
+			}
+			////////////////////////////
+			// Misc. Methods:
+			////////////////////////////
+			virtual std::string GetMessage() const = 0;				/* PVMF, makes class an abstract base class. */
+			////////////////////////////
+			// Overloaded Operators:
+			////////////////////////////
+			BaseExcept& operator=(const BaseExcept &in)				/* Assignment operator. */
+			{
+				if (this != &in)
+				{
+					message = in.message;
+				}
+				return *this;
+			}
+		};
+	}
+}
+
+
+#endif
---
+++ Options/EuropeanOption.cpp
@@ -0,0 +1,141 @@
+/* EuropeanOption.cpp
+-Description:
+	*Derived option class that uses Black-Scholes formula to give exact price.
+-Variable/Object Catalogue:
+	*normal_distribution<> normDist: Normal Distribution RNG object.
+-Member Functions:
+	// Constructors/Destructor:
+	*EuropeanOption(): Default constructor. Set all parameters to 0.
+	*EuropeanOption(double, double, double, double, double, bool): Set parameters to corresponding passed values.
+	*EuropeanOption(const EuropeanOption&): Copy constructor.
+	*~EuropeanOption(): Destructor.
+	// Misc. Methods:
+	*double D1() const: Return "d1", Z-score used in Black-Scholes model.
+	*double D2() const: Return "d2", Z-score used in Black-Scholes model.
+	*double Price() const: Return price using Black-Scholes formula.
+	*double Delta() const: Return change in Price given change in s.
+	*double Gamma() const: Return 2nd order change in Price given change in s.
+	*double Theta() const: Return change in Price given change in t.
+	*double Vega() const: Return change in Price given change in sigma.
+*/
+
+#include <boost\math\distributions.hpp>
+#include <string>
+#include <iostream>
+#include <strstream>
+#include "EuropeanOption.hpp"
+#include "OptionExcept.hpp"
+
+namespace Options
+{
+	////////////////////////////
+	// Constructors/Destructor:
+	////////////////////////////
+	EuropeanOption::EuropeanOption(double t, double s, double k, double b, double r, double sigma, bool isCall) :	/* Overloaded Constructor. Set all values of */
+	Option(t, s, k, b, r, sigma, isCall) 
+	{
+
+	}
+	EuropeanOption::EuropeanOption(const EuropeanOption &in) : Option(in)	/* Copy Constructor. */
+	{
+
+	}
+	EuropeanOption::~EuropeanOption()					/* Destructor. */
+	{
+
+	}
+	//////////////////////////
+	// Misc. Methods:
+	//////////////////////////
+	double EuropeanOption::AltPrice() const			    /* Return price of alternative type using Put-Call Parity. */
+	{
+		if (Type()) // Return price of Put option if option is Call using Put-Call Parity.
+		{
+			return Price() + Param("k") * exp(-Param("r") * Param("t")) - Param("s");
+		}
+		else		// Return price of corresponding Call option if option is Put.
+		{
+			return Price() - Param("k") * exp(-Param("r") * Param("t")) + Param("s");
+		}
+	}
+	double EuropeanOption::D1()	const					/* Calculate Z-Score used in Black-Scholes formula. */
+	{
+		// d1 = (ln(s/k) + (b + sigma^2/2) * t) / (sigma * sqrt(t))
+		return (log(Param("s") / Param("k")) + (Param("b") + .5 * pow(Param("sigma"), 2)) * Param("t")) / (Param("sigma") * sqrt(Param("t")));
+	}
+	double EuropeanOption::D2() const					/* Calculate Z-Score used in Black-Scholes formula. */
+	{
+		// d2 = d1 - sigma * sqrt(t)
+		return  D1() - Param("sigma") * sqrt(Param("t"));
+	}
+	double EuropeanOption::Price() const				/* Return price of option. */
+	{
+		try
+		{
+			if (Type())
+			{
+				return boost::math::cdf(normDist, D1()) * Param("s") * std::exp((Param("b") - Param("r")) * Param("t")) - boost::math::cdf(normDist, D2()) * Param("k") * std::exp(-Param("r") * Param("t"));
+			}
+			else
+			{
+				return boost::math::cdf(normDist, -D2()) * Param("k") * std::exp(-Param("r") * Param("t")) - boost::math::cdf(normDist, -D1()) * Param("s") * std::exp((Param("b") - Param("r")) * Param("t"));
+			}
+		}
+		catch (...)
+		{
+			throw Exceptions::OptionExcept("EuropeanOption::Price() Error: Unhandled exception. ");
+		}
+	}
+	// Greeks:
+	double EuropeanOption::Delta() const				/* Change in price with respect to s (1st order). */
+	{
+		if (Type())
+		{
+			return boost::math::cdf(normDist, D1()) * std::exp((Param("b") - Param("r")) * Param("t"));  // dC/dS = N(d1) * e^((b - r)t).
+		}
+		else
+		{
+			// Using Put-Call Parity, can see that dP/dS = dC/dS - e^((b - r))t = (N(d1) - 1) * e^((b - r)t):
+			return (boost::math::cdf(normDist, D1()) - 1) * std::exp((Param("b") - Param("r")) * Param("t"));
+		}
+	}
+	double EuropeanOption::Gamma() const				/* Change in price with respect to s (2nd order). */
+	{
+		// Following Put-Call Parity, we see that Gamma is the same for both Puts and Calls ((d/dS)^2P = (d/dS)^2C):
+		return boost::math::cdf(normDist, D1()) / (Param("s") * Param("sigma") * sqrt(Param("t")));
+	}
+	double EuropeanOption::Vega() const					/* Change in price with respect to sigma (1st order). */
+	{
+		// Following Put-Call Parity, we see that Vega is the same for both Puts and Calls (dP/dsigma = dC/dsigma):
+		return Param("k") * exp(-Param("r") * Param("t")) * boost::math::cdf(normDist, D1()) * sqrt(Param("t"));
+	}
+	double EuropeanOption::Theta() const				/* Change in price with respect to t (1st order). */
+	{
+		double outVal = 0;
+		if (Type())
+		{
+			outVal -= Param("s") * boost::math::cdf(normDist, D1()) * Param("sigma") * exp((Param("b") - Param("r")) * Param("t"));
+			outVal /= 2 * sqrt(Param("t"));
+			outVal -= (Param("b") - Param("r")) * Param("s") * exp((Param("b") - Param("r")) * Param("t")) * boost::math::cdf(normDist, D1());
+			outVal -= Param("r") * Param("k") * exp(-Param("r") * Param("t")) * boost::math::cdf(normDist, D2());
+			return outVal;
+		}
+		else
+		{
+			outVal -= (Param("s") * boost::math::cdf(normDist, D1()) * Param("sigma")) / (2 * sqrt(Param("t")));
+			outVal += Param("r") * Param("k") * exp(-Param("r") * Param("t")) * boost::math::cdf(normDist, -D2());
+			return outVal;
+		}
+	}
+	//////////////////////////
+	// Overloaded Operators: 
+	//////////////////////////
+	EuropeanOption& EuropeanOption::operator=(const EuropeanOption &in) /* Assignment Operator. */
+	{
+		if (this != &in)
+		{
+			Option::operator=(in);
+		}
+		return *this;
+	}
+}
