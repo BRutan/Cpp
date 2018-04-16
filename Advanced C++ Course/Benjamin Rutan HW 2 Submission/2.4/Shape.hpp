cf232975b9e9862a7111e6479d9df772e5f5f215
BRutan <rutan.benjamin@gmail.com>
2018-04-16 21:53:54 +0000

    Create Shape.hpp

---
+++ Advanced C++ Course/Benjamin Rutan HW 2 Submission/2.4/Shape.hpp
@@ -0,0 +1,55 @@
+/* Shape.hpp
+Description:
+	*Header file for Shape class, declares member variables/objects and functions.
+State Variables/Objects:
+	*int m_id: ID of current Shape object.
+Member Functions:
+	// Constructors/Destructor:
+	*Shape(): Default constructor. Sets m_id to random value(in between 0 and RAND_MAX).
+	*Shape(const Shape&): Copy constructor. Sets m_id to passed Shape object's m_id.
+	*virtual ~Shape(): Destructor.
+	// Accessors:
+	*int ID() const: returns the m_id of the calling Shape object.
+	// Misc. Methods:
+	*virtual void Draw() const: Output statement to stdout with calling object's information.
+	*virtual void display(IODevice&) const:
+	*virtual string ToString() const: return string description of calling Shape object.
+*/
+
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
+#include <string>
+
+class Shape
+{
+private:
+	////////////////////////////
+	// State variables: 
+	////////////////////////////
+	int m_id;											 /* ID of current Shape object. */
+public:
+	////////////////////////////
+	// Constructors/Destructor:
+	////////////////////////////
+	Shape() noexcept;									 /* Default Constructor. */
+	Shape(const Shape&) noexcept;						 /* Copy Constructor. */
+	virtual ~Shape() noexcept;							 /* Destructor. */
+	////////////////////////////
+	// Accessors:
+	////////////////////////////
+	int ID() const noexcept;							 /* Returns m_id. */
+	////////////////////////////
+	// Misc Methods:
+	////////////////////////////
+	void Print() const noexcept;						 /* Prints ToString(). */
+	virtual void Draw() const noexcept = 0;				 /* Draw the shape. */
+	virtual std::string ToString() const noexcept;				 /* Returns "ID: <m_id>". */
+	////////////////////////////
+	// Overloaded Operators:
+	////////////////////////////
+	Shape& operator=(const Shape&) noexcept;			 /* Assignment operator (with Shape). */
+};
+
+　
+#endif
