ca1ef0dab318190404772e8c557922ea977aad78
BRutan <rutan.benjamin@gmail.com>
2018-04-22 15:12:17 -0400

    Finished 2.7, just need to split exercises.

--- Advanced C++ Course/Benjamin Rutan HW 2 Submission/2.7/Main.cpp
+++ Advanced C++ Course/Benjamin Rutan HW 2 Submission/2.7/Main.cpp
@@ -1,21 +1,125 @@
-/*
-
-
-
+/* Main.cpp (exercise 2.7)
+Description:
+	* Solutions to problems 1-5.
 */
 
+#include <cerrno>
 #include <fstream>
 #include <boost\system\error_code.hpp>
+#include <string>
+#include <vector>
+#include "Wrappers.hpp"
 
 
 int main()
 {
 	/* 2.7.1 */
-	std::ifstream fileOut("..\misc.txt", std::ifstream::in);
-	if (fileOut.is_open())
+	// c) Test ReadFile with existing file and nonexisting file:
+	// File exists:
+	std::cout << "Testing Boost version: " << std::endl;
+	std::cout << "Opening file misc.txt: " << std::endl;
+	boost::system::error_condition result1(ReadFile_boost("misc.txt"));
+	std::cout << "Error message: " << result1.message() << std::endl;
+	// File does not exist:
+	boost::system::error_condition result2(ReadFile_boost("not_exist.txt"));
+	std::cout << "Opening file not_exist.txt: " << std::endl;
+	std::cout << "Error message: " << result2.message() << std::endl;
+
+	// d) Test the C++11 version of above boost ReadFile function:
+	std::cout << "Testing C++11 version: " << std::endl;
+	std::cout << "Opening file misc.txt: " << std::endl;
+	std::error_condition result3(ReadFile_cpp11("misc.txt"));
+	std::cout << "Error message: " << result3.message() << std::endl;
+	std::error_condition result4(ReadFile_cpp11("not_exist.txt"));
+	std::cout << "Opening file not_exist.txt: " << std::endl;
+	std::cout << "Error message: " << result4.message() << std::endl;
+
+	/* 2.7.2 */
+	// a) create instances of std::error_condition based on requirements:
+	// Default constructor:
+	std::error_condition err1;
+	// Value and error category:
+	std::error_condition err2(errno, std::generic_category());
+	// Using std::errc code:
+	std::error_condition err3(std::errc::file_exists);
+	
+	// b) Test the error_condition tuple returning function:
+	std::vector<std::tuple<std::string, int, std::string>> errors;
+	errors.push_back(GetCharacteristics_independent(err1));
+	errors.push_back(GetCharacteristics_independent(err2));
+	errors.push_back(GetCharacteristics_independent(err3));
+
+	// Print the characteristics of each error:
+	std::cout << "Output characteristics of error_conditions: "<< std::endl;
+	for (auto &item : errors)
 	{
-		char a = 4;
+		std::cout << "Message: " << std::get<0>(item) << std::endl;
+		std::cout << "Error Number: " << std::get<1>(item) << std::endl;
+		std::cout << "Error Category: " << std::get<2>(item) << std::endl;
 	}
+
+	/* 2.7.3 */
+	// a) Create error_condition instances using POSIX error codes in scoped enumerator errc:
+	std::error_condition err4(std::errc::io_error);
+	std::error_condition err5(std::errc::network_unreachable);
+	std::error_condition err6(std::errc::no_such_file_or_directory);
+	std::error_condition err7(std::errc::not_a_socket);
+	std::error_condition err8(std::errc::permission_denied);
+	// b) Create error_condition using value 128 and generic error category:
+	std::error_condition err9(128, std::generic_category());
+	// c) Use make_error_condition with io_errc to create instance of error_condition:
+	std::error_condition err10(std::make_error_condition(std::io_errc()));
+
+	/* 2.7.4 */
+	// a) Create default error code and platform dependent error code:
+	std::error_code default_errc;
+	std::error_code dependent_errc(0, std::generic_category());
+	// b) Test error_code characteristic returning function:
+	std::vector<std::tuple<std::string, int, std::string>> errors2;
+	errors2.push_back(GetCharacteristics_dependent(default_errc));
+	errors2.push_back(GetCharacteristics_dependent(dependent_errc));
+	std::cout << "Output characteristics of error_code: " << std::endl;
+	for (auto item : errors2)
+	{
+		std::cout << "Message: " << std::get<0>(item) << std::endl;
+		std::cout << "Error Number: " << std::get<1>(item) << std::endl;
+		std::cout << "Error Category: " << std::get<2>(item) << std::endl;
+	}
+	// c) Test equality/inequality of error_code and error_condition instances:
+	std::cout << "io_error vs network_unreachable: " << (err4 == err5) << std::endl;
+	std::cout << "io_eror vs no_such_file_or_directory: " << (err4 == err6) << std::endl;
+	std::cout << "io_error vs not_a_Socket: " << (err4 == err7) << std::endl;
+	std::cout << "io_error vs permission_denied: " << (err4 == err8) << std::endl;
+	std::cout << "io_error vs generic error: " << (err4 == err9) << std::endl;
+	std::cout << "io_error vs io_errc: " << (err4 == err10) << std::endl;
+
+	/* 2.7.5 */
+	// a-c) Adjust the provided code based upon specifications:
+	std::ifstream file(std::string("DOESNOTEXIST.txt"));
+	try 
+	{ 
+		// Set the exception mask of the file stream 
+		// In this case 1) logical error on I/O operation or 
+		// 2) read/write error on I/O operation 
+		file.exceptions(file.failbit | file.badbit); 
+	} 
+	catch (const std::ios_base::failure& e) 
+	{ 
+		std::cout << "Code: " << e.code() << ", Stream: " << std::io_errc::stream << std::endl;
+		if (e.code() == std::io_errc::stream)
+		{
+			std::error_code ec1(e.code());
+			std::error_code ec2(ec1);
+			std::tuple<std::string, int, std::string> result(GetCharacteristics_dependent(ec2));
+			std::cout << "Value: " << std::get<1>(result) << ", Message: " << std::get<0>(result) << ", Category: " << std::get<2>(result) << std::endl;
+		}
+	} 
+	catch (...) 
+	{ 
+		std::cout << "catch all\n"; 
+	}
+
 	system("pause");
+	
 	return 0;
 }
---
+++ Advanced C++ Course/Benjamin Rutan HW 2 Submission/2.7/Wrappers.hpp
@@ -0,0 +1,78 @@
+/* Wrappers.hpp (exercise 2.7.1, 2.7.2, 2.7.4)
+Description:
+	* Solutions to exercise 2.7.1, 2.7.2, 2.7.4.
+Functions:
+	* ReadFile_boost(const string &filePath) noexcept: Open file at filepath, return boost error if could not be opened.
+	* ReadFile_cpp11(const string &filePath) noexcept: Open file at filepath, return c++11 error_condition if could not be opened.
+	* GetCharacteristics_independent(const error_condition &err) noexcept: Return tuple containing message, error number and category for passed error_condition.
+	* GetCharacteristics_dependent(const error_code &err) noexcept: Return tuple containing message, error number and category for passed error_code.
+*/
+
+
+#ifndef WRAPPERS_HPP
+#define WRAPPERS_HPP
+#include <cerrno>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <boost/system/error_code.hpp>
+
+/* 2.7.1 */
+// a) Create function to read file, return boost error condition if file could not be opened:
+boost::system::error_condition ReadFile_boost(const std::string &filePath) noexcept
+{
+	std::ifstream file_in(filePath.c_str(), std::ifstream::in);
+	if (!file_in.is_open())
+	{
+		return boost::system::errc::make_error_condition(boost::system::errc::no_such_file_or_directory);
+	}
+	// If file was successfully opened then print all contents:
+	std::cout << file_in.rdbuf() << std::endl;
+	// Close file:
+	file_in.close();
+	// Return success:
+	return boost::system::errc::make_error_condition(boost::system::errc::success);
+}
+
+// d) Create C++11 version of above function:
+std::error_condition ReadFile_cpp11(const std::string &filePath) noexcept
+{
+	std::ifstream file_in(filePath.c_str(), std::ifstream::in);
+	if (!file_in.is_open())
+	{
+		return std::error_condition(errno, std::iostream_category());
+	}
+	// If file was successfully opened then print all contents:
+	std::cout << file_in.rdbuf() << std::endl;
+	// Close file:
+	file_in.close();
+	// Return success, i.e. no error code:
+	return std::error_condition();
+}
+
+/* 2.7.2 */ 
+// d) Create function to return characteristics of passed error_condition: 
+std::tuple<std::string, int, std::string> GetCharacteristics_independent(const std::error_condition &err_in) noexcept
+{
+	std::string message(err_in.message());
+	std::string category(err_in.category().name());
+	int value(err_in.value());
+	std::tuple<std::string, int, std::string> output(std::move(message), std::move(value), std::move(category));
+	return output;
+}
+
+/* 2.7.4 */
+// b) Create function to return characteristics of error_code:
+std::tuple<std::string, int, std::string> GetCharacteristics_dependent(const std::error_code &err_in) noexcept
+{
+	std::string message(err_in.message());
+	std::string category(err_in.category().name());
+	int value(err_in.value());
+	std::tuple<std::string, int, std::string> output(std::move(message), std::move(value), std::move(category));
+	return output;
+}
+
+
+#endif 
+
