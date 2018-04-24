3277dd50f7fe654e5cd0a7eb525becd126ac1831
BRutan <rutan.benjamin@gmail.com>
2018-04-24 21:28:39 +0000

    7/8s through, just finish problem 1 and split.

--- Advanced C++ Course/Benjamin Rutan HW 2 Submission/2.8/Main.cpp
+++ Advanced C++ Course/Benjamin Rutan HW 2 Submission/2.8/Main.cpp
@@ -2,8 +2,8 @@
 Description:
 	* Solutions to exercise 2.8 problems 1-5.
 
-
-
+　
+　
 */
 
 #include <bitset>
@@ -11,7 +11,9 @@ Description:
 #include <functional>
 #include <iostream>
 #include <limits>
+#include <stdexcept>
 #include <vector>
+#include "BitMatrix.hpp"
 #include "Functions.hpp"
 
 int main()
@@ -20,7 +22,7 @@ int main()
 	// a) Create bitsets from unsigned long and unsigned long long:
 	unsigned long long ull = std::numeric_limits<unsigned long long>::max(); // 8 bytes
 	unsigned long ul = std::numeric_limits<unsigned long>::max();		  // 4 bytes
-	
+
 	std::bitset<sizeof(ull)> set1(ull);
 	std::bitset<sizeof(ul)> set2(ul);
 	std::cout << "ULL maximum in binary: " << set1 << std::endl;
@@ -28,8 +30,7 @@ int main()
 	// b) Create bitsets from full strings:
 	std::bitset<sizeof(ull)> set3("01100010101");
 	std::bitset<sizeof(ul)> set4("111111101");
-	// Test CreateBitsetFromString function:
-	std::bitset<12> set5(CreateBitsetFromString<12>("0110101010101", 1));
+	std::bitset<12> set5("0110101010101", 5, 0, 5);
 	std::bitset<12> set6(435);
 	// Print results:
 	std::cout << "Bitsets generated from strings: " << std::endl;
@@ -92,9 +93,9 @@ int main()
 	std::cout << bs1 << std::endl;
 	std::cout << bs2 << std::endl;
 
-	std::cout << "XOR: " << (bs1 xor bs2) << std::endl;;
-	std::cout << "OR: " << (bs1 or_eq bs2) << std::endl;;
-	std::cout << "AND: " << (bs1 and_eq bs2) << std::endl;
+	std::cout << "XOR: " << (bs1 ^ bs2) << std::endl;;
+	std::cout << "OR: " << (bs1 | bs2) << std::endl;;
+	std::cout << "AND: " << (bs1 & bs2) << std::endl;
 	// d) Perform right and left shift operations:
 	bs1 << 1;
 	bs2 >> 2;
@@ -111,18 +112,18 @@ int main()
 	// a) Port questions 1 and 2 to use boost::dynamic_bitset<T>:
 	// Note that cannot convert an unsigned long long into a bitset (maximum size is unsigned long):
 	// boost::dynamic_bitset<> boost_set1 { sizeof(ull), ull };
-	boost::dynamic_bitset<> boost_set2 { sizeof(ul), ul };
+	boost::dynamic_bitset<> boost_set2{ sizeof(ul), ul };
 
 	std::cout << "Testing boost::dynamic_bitset<bool>: " << std::endl;
 	// std::cout << "ULL maximum in binary: " << boost_set1 << std::endl;
 	std::cout << "UL maximum in binary: " << boost_set2 << std::endl;
 	// b) Create boost::dynamic_bitsets from full strings:
-	boost::dynamic_bitset<> boost_set3 { std::string("01100010101") };
-	boost::dynamic_bitset<> boost_set4 { std::string("111111101") };
+	boost::dynamic_bitset<> boost_set3{ std::string("01100010101") };
+	boost::dynamic_bitset<> boost_set4{ std::string("111111101") };
 	// Create dynamic bitset by passing string and specifying starting index:
-	boost::dynamic_bitset<> boost_set5 { std::string("0110101010101"), 1 };
+	boost::dynamic_bitset<> boost_set5{ std::string("0110101010101"), 1 };
 	// Create dynamic bitset from passed integral literal:
-	boost::dynamic_bitset<> boost_set6 { 435 };
+	boost::dynamic_bitset<> boost_set6{ 435 };
 	// Print results:
 	std::cout << "Boost Bitsets generated from strings: " << std::endl;
 	std::cout << boost_set3 << std::endl;
@@ -133,7 +134,7 @@ int main()
 	// Test out of range exception:
 	try
 	{
-		boost::dynamic_bitset<> boost_set7 { 12, 300 };
+		boost::dynamic_bitset<> boost_set7{ 12, 300 };
 		// Note: below causes fatal run-time error, therefore cannot customize exception handling for out_of_range:
 		//boost_set7[2000];
 	}
@@ -174,6 +175,7 @@ int main()
 	// Note that bitsets must be same size for boolean comparison operators to work.
 
 	/* 2.8.2 (Boost Version) */
+	// Note: dynamic_bitset<> prints all bits, while bitset prints only up until the most significant bit.
 	// a) Create two same sized bitsets: 
 	boost::dynamic_bitset<>  boost_bs1, boost_bs2;
 	// b) Toggle all bits of bs1 and bs2:
@@ -192,15 +194,15 @@ int main()
 	std::cout << "Single left shifting on bs1: " << bs1 << std::endl;
 	std::cout << "Two right shifting on bs2: " << bs2 << std::endl;
 
-	// e) Use std::hash to created hashed values for bs1 and bs2:
-	std::hash<boost::dynamic_bitset<>> boost_hash_func();
-	std::cout << "Hash of " << boost_bs1 << ": " << hash_func(bs1) << std::endl;
-	std::cout << "Hash of " << boost_bs2 << ": " << hash_func(bs2) << std::endl;
+	// e) Use std::hash to created hashed values for boost_bs1 and boost_bs2:
+	/*std::hash<boost::dynamic_bitset<>> boost_hash_func;
+	std::cout << "Hash of " << boost_bs1 << ": " << boost_hash_func(bs1) << std::endl;
+	std::cout << "Hash of " << boost_bs2 << ": " << boost_hash_func(bs2) << std::endl;
 
 	/* 2.8.3 continued: */
 	// b) Test dynamic functionality of boost::dynamic_bitset:
 	boost::dynamic_bitset<> dynamic{ 12, 2323 };
-	
+
 	// Test resize():
 	std::cout << "For " << dynamic << std::endl;
 	dynamic.resize(13, true); // Prepends a 1 to front if true, 0 if false.
@@ -229,15 +231,58 @@ int main()
 
 	/* 2.8.4 */
 	// a) Compare vector<bool> bit related functionality to bitset's and dynamic_bitset<>'s:
-	std::vector<bool> vectorBitset; // Note: vector<bool> does not have conversion constructors of integral types to binary version.
+	std::vector<bool> vectorBitset(8, false), vectorBitset2(8, false);
+	std::bitset<8> bitset1(343), bitset2(654);
+	// Forced to use function to convert unsigned long longs to vector bitsets, since no constructor available:
+	ConvertULLToVectorBitset(vectorBitset, 343);
+	ConvertULLToVectorBitset(vectorBitset2, 654);
+	
+	std::cout << "Comparing vector bitsets vs bitsets: " << std::endl;
+	std::cout << "For " << VectorBitsetToString(vectorBitset) << " and " << VectorBitsetToString(vectorBitset2) << std::endl;
+	// Test AND:
+	std::cout << "AND bitsets: " << (bitset1 & bitset2) << std::endl;
+	std::cout << "AND vectors: " << VectorBitsetToString(AND(vectorBitset, vectorBitset2)) << std::endl;
+	// Test OR:
+	bitset1 = 343;
+	bitset2 = 654;
+	std::cout << "OR bitsets: " << (bitset1 | bitset2) << std::endl;
+	std::cout << "OR vectors: " << VectorBitsetToString(OR(vectorBitset, vectorBitset2)) << std::endl;
+	// Test XOR:
+	bitset1 = 343;
+	bitset2 = 654;
+	std::cout << "XOR bitsets: " << (bitset1 ^ bitset2) << std::endl;
+	std::cout << "XOR vectors: " << VectorBitsetToString(XOR(vectorBitset, vectorBitset2)) << std::endl;
+	// b) Test the Intersection function:
+	std::cout << "Intersection of vector bitsets " << VectorBitsetToString(vectorBitset) << " and " << VectorBitsetToString(vectorBitset2) << std::endl;
+	std::cout << VectorBitsetToString(Intersection(vectorBitset, vectorBitset2)) << std::endl;
 
-	boost::dynamic_bitset<> dynamBitset;
-	std::bitset<20> regularBitset;
-	
-	// b) Test the Intersection function, 
-	
-	
+	/* 2.8.5 */
+	// Test full functionality of BitMatrix:
+	BitMatrix<8, 8> bm1(345);
+	BitMatrix<2, 2> bm2(220);
+	BitMatrix<2, 2> bm3(121);
 
+	std::cout << "For 8 x 8 BitMatrix: " << bm1 << std::endl;
+	std::cout << "trueCount(): " << bm1.trueCount() << std::endl;
+	std::cout << "flip(): " << bm1.flip() << std::endl;
+	std::cout << "reset(): " << bm1.reset() << std::endl;
+	std::cout << "all(): " << bm1.all() << std::endl;
+	std::cout << "none(): " << bm1.none() << std::endl;
+	std::cout << "any(): " << bm1.any() << std::endl;
+	std::cout << "At(0, 0): " << bm1.At(0, 0) << std::endl;
+	try
+	{
+		std::cout << "At(9, 9): " << bm1.At(9, 9) << std::endl;
+	}
+	catch (std::out_of_range &err)
+	{
+		std::cout << "Out of range exception caught: " << err.what() << std::endl;
+	}
+	// Test boolean operators:
+	std::cout << "For BitMatrices: " << bm2 << "\n and " << bm3 << ":" << std::endl;
+	std::cout << "AND: " << (bm2 & bm3) << std::endl;
+	std::cout << "OR: " << (bm2 | bm3) << std::endl;
+	std::cout << "XOR: " << (bm2 ^ bm3) << std::endl;
 
 	system("pause");
 
