49b048b0f3b15136cf271592cf99f93cd1c28c27
BRutan <rutan.benjamin@gmail.com>
2018-04-30 18:03:34 -0400

    Add files via upload

---
+++ Advanced C++ Course/Benjamin Rutan HW 3 Submission/3.1/3.1.4/3.1.4/Main.cpp
@@ -0,0 +1,22 @@
+/* Main.cpp (exercise 3.1.4)
+Description:
+	* 
+
+
+*/
+
+
+#include <iostream>
+#include <thread>
+#include <vector>
+#include "ProducerConsumer.hpp"
+#include "SynchronisedQueue.hpp"
+
+int main()
+{
+	std::vector<std::thread> producers, consumers;
+
+
+
+	return 0;
+}
