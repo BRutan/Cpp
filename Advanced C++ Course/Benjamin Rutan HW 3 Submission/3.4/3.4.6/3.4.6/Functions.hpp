0541c36800d856cb0610b3f3c9e41ca8c5bc17b1
BRutan <rutan.benjamin@gmail.com>
2018-05-20 14:40:29 -0400

    Begin 3.4.6, need to finish.

---
+++ Advanced C++ Course/Benjamin Rutan HW 3 Submission/3.4/3.4.6/3.4.6/Functions.hpp
@@ -0,0 +1,19 @@
+/* Functions.hpp (exercise 3.4.6)
+Description:
+	* 
+
+
+
+*/
+
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+
+void Function()
+{
+
+}
+
+
+
+#endif
---
+++ Advanced C++ Course/Benjamin Rutan HW 3 Submission/3.4/3.4.6/3.4.6/Main.cpp
@@ -0,0 +1,16 @@
+/* Main.cpp (exercise 3.4.6)
+Description:
+	*
+
+
+
+*/
+
+
+int main()
+{
+
+
+
+	return 0;
+}
